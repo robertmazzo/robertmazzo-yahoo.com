ced6d8c3ea267ffbc68ada6d78f7963b6fd52818
Bob Mazzo <robertmazzo@yahoo.com>
2016-07-14 21:03:38 +0000

    gig table commit, and override conventions to change nullable columns.

---
+++ GigHub/Models/Gig.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+namespace GigHub.Models
+{
+    /// <summary>
+    ///  ***NB: we need to associate this class with another class
+    /// that represents a user in GigHub; see ApplicationUser in IdentityModels
+    /// </summary>
+    public class Gig
+    {
+        public int Id { get; set; }
+
+        [Required]
+        public ApplicationUser Artist { get; set; }
+
+        public DateTime DateTime { get; set; }
+
+        [Required]
+        [StringLength(255)]
+        public string Venue { get; set; }
+
+        [Required]
+        public Genre Genre
+        { get; set; }
+    }    
+}
